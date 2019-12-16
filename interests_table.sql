862807d636044bb2a91146dbaa2332738a769485
wrightb87 <46726515+wrightb87@users.noreply.github.com>
2019-12-16 12:28:46 -0700

    Delete interests_table.sql

--- interests_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS interests(
-id SERIAL PRIMARY KEY
-interests VARCHAR(50)
-);
-
-INSERT INTO interests(interests)
-VALUES('Web Dev'),('Blog'),('Databases'),('Automation'),('Authentication'),
-('NONE'),('API Usage'),('Mobile Dev'),('Ethical Hacking'),('Penetration Testing'),
-('Machine Learning'),('Data Science');
